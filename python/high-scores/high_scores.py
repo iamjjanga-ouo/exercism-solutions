4d3784c23a15cf159251b052f9dc18a6d2ebfec3
iamjjanga-ouo <namils147@kakao.com>
2020-12-15 13:02:16 +0900

    +python-high-scores

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,14 @@
+# scores is list
+
+def latest(scores):
+    return scores[-1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    return sorted(scores, reverse=True)[:3]
+
+
