283b24631a7badbe41a9c7a76ceb847800fd3b00
iamjjanga-ouo <namils147@kakao.com>
2020-12-28 08:45:35 +0900

    +python-pangram: lower case using string.ascii_lowercase

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,6 +1,8 @@
+from string import ascii_lowercase
+
 def is_pangram(sentence):
     # a-z character list
-    compared = [chr(n) for n in range(97, 123)]
+    compared = list(ascii_lowercase)
     # only alphabet list in sentence
     res = [n for n in (sentence.lower()).replace(' ', '') if n.isalpha()]
 
