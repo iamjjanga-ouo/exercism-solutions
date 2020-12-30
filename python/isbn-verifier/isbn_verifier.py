c418e514813a4ac32e6f1816c0d1b722f2569b50
iamjjanga-ouo <namils147@kakao.com>
2020-12-30 14:32:25 +0900

    +python-isbn-vertifier: refactoring with list comprehension

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,24 +1,14 @@
 import re
 
 def is_valid(isbn):
-    wo_isbn = isbn.replace("-", "")
+    wo_isbn = isbn.replace("-", "") # delete hypen
+
     if len(wo_isbn) != 10:
         return False
-    if not bool(re.match("[0-9X]{10}", wo_isbn)):
-        return False
+    if not bool(re.match("[0-9]{9}[0-9X]", wo_isbn)): # type only 9 digits + check Character 'X' (locate only last in the words)
+        return False                                  # or 10 digits
 
-    sum = 0
-    for i, n in enumerate(isbn.replace("-", "")[::-1]):
-        if i !=0 and n == 'X':
-            return False
-        if n == 'X':
-            n = '10'
-        sum += int(n) * (i+1)
+    res = sum([10*(i+1) if n == 'X' else int(n)*(i+1)
+               for i, n in enumerate(wo_isbn[::-1])])
 
-    print(sum)
-    print(sum % 11)
-    return bool(sum % 11 == 0)
-
-
-if __name__ == '__main__':
-    is_valid("3-598-2X507-9")
+    return bool(res % 11 == 0)
