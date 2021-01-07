b6e4dabda5f5458c561f260f4a7302ad4578c6fa
iamjjanga-ouo <namils147@kakao.com>
2021-01-08 08:45:43 +0900

    +python-meetup: solve 'teenth'

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -2,8 +2,8 @@ import calendar
 from datetime import date
 
 class MeetupDayException(Exception):
-    def __str__(self):
-        return "day is out of range for month"
+    def __init__(self):
+        super().__init__("out of range in month")
 
 
 weekdays = [ "Monday",
@@ -20,8 +20,8 @@ def meetup(year, month, week, day_of_week):
         month_list = calendar.monthcalendar(year, month)
         weekday = weekdays.index(day_of_week)
         if week == "teenth":
-            for w in month_list:
-                if w[weekday] >= 10:
+            for w in month_list[::-1]:
+                if 10 <= w[weekday] and w[weekday] < 20:
                     return date(year, month, w[weekday])
         elif week == "1st":
             for w in month_list:
@@ -40,16 +40,18 @@ def meetup(year, month, week, day_of_week):
                 if w[weekday] != 0:
                     return date(year, month, w[weekday] + 21)
         elif week == "5th":
-            if month_list[-1][weekday] != 0:
-                return date(year, month, month_list[4][weekday])
+            if month_list[5][weekday] != 0:
+                return date(year, month, month_list[5][weekday])
         elif week == "last":
             for w in month_list[::-1]:
                 if w[weekday] != 0:
                     return date(year, month, w[weekday])
-    except ValueError:
-        raise MeetupDayException
+        else:
+            raise MeetupDayException
+    except Exception as e:
+        print("Exception : ", e)
 
 
 if __name__ == '__main__':
+    # print(calendar.prmonth(2015, 3))
     print(meetup(2015, 2, "5th", "Monday"))
-    print(calendar.prmonth(2015, 2))
