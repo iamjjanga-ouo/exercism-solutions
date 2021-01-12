371a91e98577a145b686cbedb5732d63240944db
iamjjanga-ouo <namils147@kakao.com>
2021-01-12 11:52:28 +0900

    +bash : reverse_string.sh + Add another methods

--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+## 1
 # len="${#1}" is better than echo "$1" | wc -c (SC20000)
 main() {
   len="${#1}"
-  string="$1"
   for((i=len; i>=0; i--)); do
-    c="${string:$i:1}"
+    c="${1:$i:1}"
     echo -n "$c"
   done
 }
+
+## 2
+#main() {
+#  for ((i = 1; i<=${#1}; i++)); do
+#	  printf "%c" "${1: -$i}" # 역순 인덱스에서
+#  done
+#}
+
+## 3
+#main() {
+#  echo "$1" | rev
+#}
 
 main "$@"
