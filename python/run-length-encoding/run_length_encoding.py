e6253e62ffdfe1ecd5d4b02979fc5066fb103284
iamjjanga-ouo <namils147@kakao.com>
2021-01-13 09:30:16 +0900

    +python: re-code run_length_encoding.py using re.sub + lambda

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,49 +1,45 @@
+from re import sub
+
 def decode(string):
-    num = 0
-    de_str = ""
-
-    # Check String is empty
-    if not string:
-        return string
-    # Check each Character in string
-    for c in string:
-        if c.isdigit(): # If character is digit, count number
-            num *= 10
-            num += int(c)
-        else:               # If character is letter, append character * count in de_str
-            if num == 0:
-                de_str += c
-            else:
-                de_str += c * num
-            num = 0
-
-    return de_str
+    return sub(r'(\d+)(\D)', lambda x: int(x.group(1)) * x.group(2), string)
+# (\d+)(\D)는 1개이상의 숫자 + 문자를 의미
+# 만약 12WB12W3B24WB 라면 12W,12W,3B,24W만 매칭되어서 lambda함수를 통해 변형된다.
+# 매칭되지 않는 값들은 그대로 있음.
 
 def encode(string):
-    en_str = ""
+    return sub(r'(.)\1+', lambda x: str(len(x.group(0))) + x.group(1), string)
+# (.)\1+ 같의 글자의 반복을 의미
+# group(0)은 매칭된 문자 전체를 return
 
-    # Check String is empty
-    if not string:
-        return string
 
-    string += "." # Character end of string
-    i = 1
-    while True:
-        if string[i] == ".": # Break Loop if end of string
-            break
-        if string[i-1] != ',' and string[i] != string[i-1]:
-            string = string[:i] + "," + string[i:]
-        i += 1
 
-    string = string[:len(string)-1] # delete character end of string
 
-    for i in string.split(","):
-        if len(i) == 1:
-            en_str += i[0]
-        else:
-            en_str += str(len(i)) + i[0]
 
-    return en_str
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ########################
